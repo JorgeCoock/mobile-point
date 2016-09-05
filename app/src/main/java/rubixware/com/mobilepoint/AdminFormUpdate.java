54a3f5a74573734716e9f63f2640db9526cb1afe
Jorge Garcia <hegg777@gmail.com>
2016-09-04 22:11:05 -0500

    Admin form update activity created!

---
+++ app/src/main/java/rubixware/com/mobilepoint/AdminFormUpdate.java
@@ -0,0 +1,21 @@
+package rubixware.com.mobilepoint;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+
+public class AdminFormUpdate extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_form_update);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+}

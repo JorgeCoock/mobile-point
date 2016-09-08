de1c27aeeb924fff1dcad61fe9d57d83d5103c5d
Zavala29 <mario290696@gmail.com>
2016-09-07 20:43:42 -0500

    Make this file

--- app/src/main/java/rubixware/com/mobilepoint/Page.java
+++ app/src/main/java/rubixware/com/mobilepoint/Page.java
@@ -1,5 +1,18 @@
 package rubixware.com.mobilepoint;
 
-public class Page{
-    
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import java.util.ArrayList;
+
+
+public class Page {
+    public static final String TABLE_PAGES = "pages";
+    public static final String COLUMN_PAGES_ID = "id";
+    public static final String COLUMN_PAGES_USERNAME = "userpage";
+
+
+    public AdminQueries() {}
+
+
 }

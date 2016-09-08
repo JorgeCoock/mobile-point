34af3c22011340d89585daa53db87fc54e08ac67
Zavala29 <mario290696@gmail.com>
2016-09-07 20:58:35 -0500

    Make the part of ArrayList

--- app/src/main/java/rubixware/com/mobilepoint/Page.java
+++ app/src/main/java/rubixware/com/mobilepoint/Page.java
@@ -24,7 +24,7 @@ public class Page {
     }
 
     public Pages showPages(String adminName, DataBaseHelper dbHelper){
-        String query = "Select * FROM " + TABLE_PAGES + " WHERE " + COLUMN_PAGES_USERNAME + " =  \"" + adminName + "\"";
+        String query = "Select * FROM " + TABLE_PAGES + " WHERE " + COLUMN_PAGES_USERNAME + " =  \"" + pagesName + "\"";
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         Cursor cursor = db.rawQuery(query, null);
         Admin admin = new Admin();
@@ -41,5 +41,27 @@ public class Page {
         db.close();
         return pages;
     }
-
+    public ArrayList<Pages> getPagesIndex(DataBaseHelper dbHelper){
+        ArrayList<Pages> pagesList = new ArrayList<Pages>();
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        try{
+            Cursor cursor = db.rawQuery("Select * FROM "+TABLE_PAGES, null);
+            try{
+                if (cursor.moveToFirst()){
+                    do {
+                        Page pages = new Pages();
+                        pages.setId(Integer.parseInt(cursor.getString(0)));
+                        pages.setUsername(cursor.getString(1));
+                        pages.setPassword(cursor.getString(2));
+                        pagesList.add(admin);
+                    }while (cursor.moveToNext());
+                }
+            } finally {
+                try { cursor.close();} catch (Exception ignore) {}
+            }
+        } finally {
+            try{ db.close(); } catch (Exception ignore) {}
+        }
+        return pagesList;
+    }
 }

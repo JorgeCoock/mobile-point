7d69f81252851848feab5ab2091caa609bf604a6
Jorge Garcia <hegg777@gmail.com>
2016-09-08 22:33:15 -0500

    Set listview text on create on panel users option

--- app/src/main/java/rubixware/com/mobilepoint/PanelUsersOption.java
+++ app/src/main/java/rubixware/com/mobilepoint/PanelUsersOption.java
@@ -1,14 +1,20 @@
 package rubixware.com.mobilepoint;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class PanelUsersOption extends Activity{
+
+    String username;
+    Intent intent;
+    TextView editUserTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,10 @@ public class PanelUsersOption extends Activity{
         setContentView(R.layout.panel_users_option);
         Typeface font = Typeface.createFromAsset(getAssets(), "fontawesome-webfont.ttf");
         setFaIconsToButtons(font);
+        intent = getIntent();
+        username = intent.getStringExtra("username");
+        editUserTitle = (TextView) findViewById(R.id.edit_user_title);
+        editUserTitle.setText(username);
     }
 
     @Override
